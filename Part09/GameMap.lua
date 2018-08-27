f185ee3ae8faacf86f917f2eba6be63da64c1dac
CliffsDover <bottle@dancingbottle.com>
2018-08-27 17:33:48 +0800

    revert fireball damage

--- Part09/GameMap.lua
+++ Part09/GameMap.lua
@@ -220,11 +220,11 @@ function GameMap:place_entities( room, entities, max_monsters_per_room, max_item
             local item_chance = love.math.random( 100 )
             local item_component
             local item
-            if item_chance < 10 then
+            if item_chance < 70 then
                 item_component = Item( heal, { amount = 4 } )
                 item =  Entity( x, y, '藥', COLORS.LIGHTEST_VIOLET, '藥水', false, RENDER_ORDER.ITEM, nil, nil, item_component )
             elseif item_chance < 85 then
-                item_component = Item( cast_fireball, { damage = 100, radius = 3 }, true, Message( "按滑鼠左鍵以選擇目標來發射火球，或按滑鼠右鍵取消。", COLORS.CYAN ) )
+                item_component = Item( cast_fireball, { damage = 12, radius = 3 }, true, Message( "按滑鼠左鍵以選擇目標來發射火球，或按滑鼠右鍵取消。", COLORS.CYAN ) )
                 item =  Entity( x, y, '卷', COLORS.RED, '火球卷軸', false, RENDER_ORDER.ITEM, nil, nil, item_component )
             else
                 item_component = Item( cast_lightning, { damage = 20, maximum_range = 5 } )
